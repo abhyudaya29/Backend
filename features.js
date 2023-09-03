83edfa8148e88c649de46eeab77a614e28cdf1a0
abhyudaya29 <abhyudayadev123@gmail.com>
2023-09-04 04:27:58 +0530

    exprress

---
+++ features.js
@@ -0,0 +1,14 @@
+const gfname="Mrs random";
+
+
+let abhyudaya="Dubey";
+
+let dev="dubey"
+export default gfname
+
+export{abhyudaya,dev}
+
+
+export let baby=()=>{
+    return `${Math.floor(Math.random()*100)}%`
+}
